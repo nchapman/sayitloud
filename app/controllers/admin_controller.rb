98319f99b587d4b461ebf9f34784b74bcf06f3c7
nchapman <nchapman@gmail.com>
2010-03-29 09:20:17 -0700

    Added top 10 list to admin index.

--- app/controllers/admin_controller.rb
+++ app/controllers/admin_controller.rb
@@ -2,5 +2,6 @@ class AdminController < ApplicationController
   before_filter :require_user
   
   def index
+    @top_subdomains_today = Subdomain.find(:all, :limit => 10, :order => "views_today DESC", :include => "site")
   end
 end
