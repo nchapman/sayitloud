aed1f49239ea5f4a411bb37f6750debc4d78d1be
nchapman <nchapman@gmail.com>
2010-03-29 09:31:15 -0700

    Improved top list.

--- app/controllers/admin_controller.rb
+++ app/controllers/admin_controller.rb
@@ -2,6 +2,6 @@ class AdminController < ApplicationController
   before_filter :require_user
   
   def index
-    @top_subdomains_today = Subdomain.find(:all, :limit => 10, :order => "views_today DESC, views_all_time DESC", :include => "site")
+    @top_subdomains_today = Subdomain.find(:all, :limit => 10, :order => "views_today DESC", :conditions => "views_today > 0", :include => "site")
   end
 end
