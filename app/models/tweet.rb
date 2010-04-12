7e719331ab292db492159e4615431b50946c64b3
nchapman <nchapman@gmail.com>
2010-04-11 17:42:41 -0700

    Tweet uniqueness improvements.

--- app/models/tweet.rb
+++ app/models/tweet.rb
@@ -1,6 +1,5 @@
 class Tweet < ActiveRecord::Base
   belongs_to :site
-  validates_uniqueness_of :twitter_id
   
   def self.update_from_twitter
     Site.all.each do |site|
@@ -10,15 +9,15 @@ class Tweet < ActiveRecord::Base
       search.since(last_tweet.twitter_id) if last_tweet
       
       search.each do |r|
-        tweet = site.tweets.new(
-          :twitter_id => r["id"],
-          :user => r["from_user"],
-          :profile_image_url => r["profile_image_url"],
-          :body => CGI.escapeHTML(r["text"]),
-          :originally_created_at => r["created_at"]
-        )
-        
-        tweet.save if tweet.valid?
+        unless Tweet.find_by_twitter_id(r[id])
+          site.tweets.create(
+            :twitter_id => r["id"],
+            :user => r["from_user"],
+            :profile_image_url => r["profile_image_url"],
+            :body => CGI.escapeHTML(r["text"]),
+            :originally_created_at => r["created_at"]
+          )
+        end
       end
     end
   end
